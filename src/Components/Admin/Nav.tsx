1676abff873e811ae049ee140084e51a0312d296
Adarsh <adarshjain9628@gmail.com>
2020-04-29 18:04:19 +0530

    Update deps in navbar

--- src/Components/Admin/Nav.tsx
+++ src/Components/Admin/Nav.tsx
@@ -1,30 +1,30 @@
 import React, {useEffect} from 'react';
 import {IndexPath, Menu, MenuItem} from '@ui-kitten/components';
-import {useHistory, useLocation, useRouteMatch} from "../../Routers/routing";
+import {useHistory, useLocation} from "../../Routers/routing";
 import AdminRoutes from "../../Routers/AdminRoutes";
 
 export const Nav = () => {
 
     const [selectedIndex, setSelectedIndex] = React.useState<IndexPath | undefined>(undefined);
-    let {url} = useRouteMatch();
     let history = useHistory();
     let loc = useLocation();
     let adminRoutes = AdminRoutes.items;
+
+    useEffect(() => {
+        if (loc.pathname === "/admin") {
+            let i = AdminRoutes.getPathFromIndex(0);
+            history.replace(i);
+            loc.pathname = i;
+        }
+        console.log('sdf');
+        let indexPath: IndexPath = new IndexPath(AdminRoutes.getIndexFromPath(loc.pathname));
+        setSelectedIndex(indexPath);
+    }, [loc.pathname, history]);
 
     function onSelectMenu(index: IndexPath) {
         history.push(`${AdminRoutes.getPathFromIndex(index.row)}`);
         setSelectedIndex(index);
     }
-
-    useEffect(() => {
-        if(loc.pathname === "/admin"){
-            let i = AdminRoutes.getPathFromIndex(0);
-            history.replace(i);
-            loc.pathname = i;
-        }
-        let indexPath: IndexPath = new IndexPath(AdminRoutes.getIndexFromPath(loc.pathname));
-        setSelectedIndex(indexPath);
-    }, []);
 
     return (
         <Menu
