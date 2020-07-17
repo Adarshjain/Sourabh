3bd9d5d9922dce6138bdb5f64968f59a1cbd8769
cb-adarsh <adarsh@chargebee.com>
2020-07-17 20:25:03 +0530

    'graphql endpoint update'

--- src/Network/Apollo/client.ts
+++ src/Network/Apollo/client.ts
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
     uri: '/graphql',
-    // uri: 'http://lancincatalog.me/graphql',
+    // uri: 'http://suniljewellerscatalog-env.eba-qzd8ixja.ap-south-1.elasticbeanstalk.com/graphql',
     request: (operation) => {
         const token = localStorage.getItem('logTok');
             operation.setContext({
